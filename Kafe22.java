81351911fdb9172ff4a45776520a503bba104c9f
rizdannn <rafifprastana@gmail.com>
2025-11-28 16:13:26 +0700

    Percobaan 2

--- Kafe22.java
+++ Kafe22.java
@@ -1,5 +1,10 @@
 public class Kafe22 { 
-    public static void main(String[] args) {
+    public static void menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!" ) ;
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap poembelian!");
+        }
         System.out.println("====== MENU RESTO KAFE ======");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -9,5 +14,9 @@ public class Kafe22 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===============================");
         System.out.println("Silakan pilih menu yang Anda inginkan:");
+        
+    }
+    public static void main(String[] args) {
+        menu("andi",true);
     }
 }
