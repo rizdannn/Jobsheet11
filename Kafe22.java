51ce9383d055dfe94811e813559aa3611db78b59
rizdannn <rafifprastana@gmail.com>
2025-11-28 10:08:44 +0700

    Modifikasi Percobaan 1

--- Kafe22.java
+++ Kafe22.java
@@ -1,5 +1,5 @@
-public class Kafe22 {
-    public static void menu () {
+public class Kafe22 { 
+    public static void main(String[] args) {
         System.out.println("====== MENU RESTO KAFE ======");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -9,9 +9,5 @@ public class Kafe22 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===============================");
         System.out.println("Silakan pilih menu yang Anda inginkan:");
-        
-    }
-    public static void main(String[] args) {
-        menu();
     }
 }
