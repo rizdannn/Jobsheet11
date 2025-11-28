17e7da6918a119f66625c1b010f40a272b3b418f
rizdannn <rafifprastana@gmail.com>
2025-11-28 16:46:53 +0700

    Modifikasi Percobaan 2

--- Kafe22.java
+++ Kafe22.java
@@ -16,7 +16,24 @@ public class Kafe22 {
         System.out.println("Silakan pilih menu yang Anda inginkan:");
         
     }
+    public static void hitungTotal(double totalBelanja, String kodePromo) {
+
+         System.out.println("Total belanja: Rp " + totalBelanja);
+
+        if (kodePromo.equals("DISKON50")) {
+            double diskon = totalBelanja * 0.50;
+            System.out.println("Diskon 50% diberikan: Rp " + diskon);
+        } else if (kodePromo.equals("DISKON30")) {
+            double diskon = totalBelanja * 0.30;
+            System.out.println("Diskon 30% diberikan: Rp " + diskon);
+        } else {
+            System.out.println("Kode promo invalid");
+        }
+     
+}
     public static void main(String[] args) {
         menu("andi",true);
+        System.out.print("\n");
+        hitungTotal(100000, "DISKON50");
     }
 }
