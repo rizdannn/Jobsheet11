23e2119e11f157885d2fc7784a3a701c8961e1a6
rizdannn <rafifprastana@gmail.com>
2025-12-04 00:49:37 +0700

    Percobaan 4

---
+++ PengunjungCafeNoPresensi.java
@@ -0,0 +1,16 @@
+public class PengunjungCafeNoPresensi {
+    
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
