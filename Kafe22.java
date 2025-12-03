928a7904f4fd57d2c436f930b3977f24b776c1f5
rizdannn <rafifprastana@gmail.com>
2025-12-04 00:49:07 +0700

    Modifikasi Percobaan 3

--- Kafe22.java
+++ Kafe22.java
@@ -28,13 +28,25 @@ public class Kafe22 {
         
         }
 
-        public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
-            int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000}; 
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem, int jumlahItemBaru, String kodePromo) {
 
-            int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-            return hargaTotal;
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        int diskon = 0;
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid: DISKON50");
+            diskon = hargaTotal / 2;
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: DISKON30");
+            diskon = (int)(hargaTotal * 0.3);
+        } else {
+            System.out.println("Kode promo invalid.");
         }
-    
+
+    return hargaTotal - diskon;
+}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -46,9 +58,32 @@ public class Kafe22 {
         System.out.print("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
 
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+        String pesanLagi = "";
 
-        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
+        do {
+            System.out.print("\nApakah ingin memesan menu lain? (y/n): ");
+            pesanLagi = sc.next();
+
+            if (pesanLagi.equalsIgnoreCase("y") || pesanLagi.equalsIgnoreCase("ya")) {
+
+                System.out.print("Masukkan nomor menu yang ingin Anda pesan: ");
+                int pilihanMenuBaru = sc.nextInt();
+
+                System.out.print("Masukkan jumlah item: ");
+                int jumlahItemBaru = sc.nextInt();
+
+                System.out.print("Masukkan kode promo: ");
+                String kodePromoBaru = sc.next();
+
+                int totalBaru = hitungTotalHarga(pilihanMenuBaru, jumlahItemBaru, jumlahItemBaru, kodePromoBaru);
+
+                System.out.println("Total harga pesanan: Rp" + totalBaru);
+            }
+
+        } while (pesanLagi.equalsIgnoreCase("y") || pesanLagi.equalsIgnoreCase("ya"));
+
+        System.out.println("\nTerima kasih sudah memesan!");
+
 
     }
 }
